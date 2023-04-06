12ce9f578e78fcb7885c0f9dd08d2130aabc79f8
Christopher Hlubek <christopher.hlubek@networkteam.com>
2023-04-06 16:48:50 +0200

    Update GH actions

--- example/pgx/repository/mappings.go
+++ example/pgx/repository/mappings.go
@@ -1,4 +1,3 @@
 package repository
 
-//go:generate go run github.com/networkteam/construct/v2/cmd/construct github.com/networkteam/construct/v2/example/pgx/model.Project
-//go:generate go run github.com/networkteam/construct/v2/cmd/construct github.com/networkteam/construct/v2/example/pgx/model.Todo
+//go:generate go run github.com/networkteam/construct/v2/cmd/construct github.com/networkteam/construct/v2/example/pgx/model
